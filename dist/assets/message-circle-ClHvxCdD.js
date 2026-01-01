9475961cfc6e60538e4b8322dd4413ef0d07c458
konthamsohith <konthamsohith2@gmail.com>
2026-01-01 21:18:38 +0530

    Fix TypeScript unused import in Contact.tsx causing build failure

---
+++ dist/assets/message-circle-ClHvxCdD.js
@@ -0,0 +1,11 @@
+import{c as e}from"./index-CahSo0n6.js";/**
+ * @license lucide-react v0.344.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=e("Calendar",[["path",{d:"M8 2v4",key:"1cmpym"}],["path",{d:"M16 2v4",key:"4m81vk"}],["rect",{width:"18",height:"18",x:"3",y:"4",rx:"2",key:"1hopcy"}],["path",{d:"M3 10h18",key:"8toen8"}]]);/**
+ * @license lucide-react v0.344.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=e("MessageCircle",[["path",{d:"M7.9 20A9 9 0 1 0 4 16.1L2 22Z",key:"vv11sd"}]]);export{t as C,c as M};
